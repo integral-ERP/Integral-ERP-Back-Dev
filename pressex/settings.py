8029e48a68f2741621d90616d222f09fe0886a2f
Alejandro Narvaez Fajardo <alejonarvaez3489@gmail.com>
2023-09-11 11:01:34 -0500

    Migraciones y paginacion
    Instalamos core api para presentar el formato de documentacion y subimos migraciones
    
    Co-Authored-By: Cristian Tole <51107661+cristiantodi@users.noreply.github.com>

--- pressex/settings.py
+++ pressex/settings.py
@@ -174,5 +174,9 @@ AUTH_USER_MODEL = 'user.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+   
+    
 }
