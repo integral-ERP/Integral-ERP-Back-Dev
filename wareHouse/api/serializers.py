dcb8039fac32d5fa6115f8480c5a35bc55262dbc
Alejandro Narvaez Fajardo <alejonarvaez3489@gmail.com>
2023-09-25 17:12:59 -0500

    feat:back-end con dbsqlite
    acomodando migraciones ycampo inland_carrier

--- wareHouse/api/serializers.py
+++ wareHouse/api/serializers.py
@@ -20,7 +20,7 @@ class PickUpOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PickUpOrder
        
-        fields = [  'status','number','creation_date','pick_up_date','delivery_date','date','issued_by','issued_byObj','destination_agent','destination_agentObj','employee','employeeObj','shipper','shipperObj','pick_up_location','pickUpLocationObj','consignee','consigneeObj','delivery_location','deliveryLocationObj','inland_carrier','in_land_carrierObj','main_carrier','main_carrierObj','pro_number','tracking_number','supplier','supplierObj','invoice_number','purchase_order_number'
+        fields = [  'status','number','creation_date','pick_up_date','delivery_date','date','issued_by','issued_byObj','destination_agent','destination_agentObj','employee','employeeObj','shipper','shipperObj','pick_up_location','pickUpLocationObj','consignee','consigneeObj','delivery_location','deliveryLocationObj','inland_carrier','inland_carrierObj','main_carrier','main_carrierObj','pro_number','tracking_number','supplier','supplierObj','invoice_number','purchase_order_number'
                 ] 
  
 
