527c29ca35d32aa6a0982f1762460e6bb58d1eb4
unknown <toleko89@hotmail.com>
2024-06-04 10:33:07 -0500

    :v: Agregando valor volume en WH

--- wareHouse/api/views.py
+++ wareHouse/api/views.py
@@ -18,7 +18,7 @@ class PickUpOrderApiViewSet(BaseModelViewSet):
     serializer_class = PickUpOrderSerializer
     queryset = PickUpOrder.objects.filter(disabled=False).select_related('employee')
     filter_backends = [filters.SearchFilter]
-    search_fields = ['status','number','creation_date','pick_up_date','delivery_date','date','issued_by','destination_agent','employee','shipper','pick_up_location','consignee','delivery_location','inland_carrier','main_carrier','pro_number','tracking_number','supplier','invoice_number','purchase_order_number']
+    search_fields = ['status','number','creation_date','pick_up_date','delivery_date','date','issued_by','destination_agent','employee','shipper','pick_up_location','consignee','delivery_location','inland_carrier','main_carrier','pro_number','tracking_number','supplier','invoice_number','purchase_order_number','volumen','weight',]
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -30,7 +30,7 @@ class ReceptionOrderApiViewSet(BaseModelViewSet):
     serializer_class = ReceptionOrderSerializer
     queryset = ReceptionOrder.objects.filter(disabled=False).select_related('employee')
     filter_backends = [filters.SearchFilter]
-    search_fields = ['status','number','creation_date','employee','issued_by','destination_agent','shipper','consignee','client_to_bill','main_carrier','commodities','events','attachments','weight']
+    search_fields = ['status','number','creation_date','employee','issued_by','destination_agent','shipper','consignee','client_to_bill','main_carrier','commodities','events','attachments','volumen','weight',]
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
